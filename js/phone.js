41a43915fdf99f690371ea22f6e05feee7c74cce
kqd666 <97704420+kqd666@users.noreply.github.com>
2023-05-20 22:45:56 +0300

    Add files via upload

---
+++ js/phone.js
@@ -0,0 +1,11 @@
+VANTA.WAVES({
+  el: "#intro1",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x2d2d2d
+})
